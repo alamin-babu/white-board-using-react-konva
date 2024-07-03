79c13459ba1372783ad1e6e9fda1efa6e2b9206d
kiniokinilo <alamin@duck.com>
2024-07-03 08:52:49 +0600

    ok

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Whiteboard from './Whiteboard';
+
+function App() {
+  return (
+    <div className="App" id='uu'>
+      <Whiteboard/>
+    </div>
+  );
+}
+
+export default App;
